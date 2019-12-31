47c27826ddb47c1419ccd9cadbe342c719f8ba48
Matthew Bonig <matthew.bonig@gmail.com>
2019-12-31 09:39:49 -0700

    cleaning up submodules

---
+++ src/themes/hugo-tailwind-journal/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      flex: {
+        'l-24': '0 0 6rem'
+      },
+      fontFamily: {
+        serif: ['Nanum Myeongjo'],
+        mono: ['Source Code Pro']
+      },
+      padding: {
+        '1/2': '0.125rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+};
