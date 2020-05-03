e1c4196ef261aca79ba59b0be07767ed4d0b9b79
Matthew Bonig <matthew.bonig@gmail.com>
2020-05-03 11:16:03 -0600

    adding CF and cert

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,22 +1,32 @@
-import {Bucket, BucketAccessControl, BucketEncryption, RedirectProtocol} from '@aws-cdk/aws-s3';
+import {Bucket, BucketEncryption, RedirectProtocol} from '@aws-cdk/aws-s3';
 import {Construct, SecretValue, Stack, StackProps} from '@aws-cdk/core';
 import {HostedZone, RecordSet, RecordTarget, RecordType} from '@aws-cdk/aws-route53';
 
-import {BucketWebsiteTarget} from '@aws-cdk/aws-route53-targets'
+import {CloudFrontTarget} from '@aws-cdk/aws-route53-targets'
 import {Artifact, Pipeline} from "@aws-cdk/aws-codepipeline";
 import {CodeBuildAction, GitHubSourceAction, S3DeployAction} from "@aws-cdk/aws-codepipeline-actions";
 import {BuildSpec, ComputeType, LinuxBuildImage, PipelineProject} from '@aws-cdk/aws-codebuild';
+import {DnsValidatedCertificate} from "@aws-cdk/aws-certificatemanager";
+import {CloudFrontWebDistribution, OriginAccessIdentity, ViewerCertificate} from "@aws-cdk/aws-cloudfront";
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 
+export interface InfraStackProps extends StackProps {
+    hostedZoneId: string;
+}
 
 export class InfraStack extends Stack {
     websiteBucket: Bucket;
     redirectBucket: Bucket;
     private buildArtifactBucket: Bucket;
     private buildSpec: any;
+    private distribution: CloudFrontWebDistribution;
+    private props: InfraStackProps;
+    private oai: OriginAccessIdentity;
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
+    constructor(scope: Construct, id: string, props: InfraStackProps) {
         super(scope, id, props);
 
+        this.props = props;
         // The code that defines your stack goes here
         this.setupBucket();
         this.setupRoute53();
@@ -32,39 +42,76 @@ export class InfraStack extends Stack {
             publicReadAccess: false
         });
 
+
         this.websiteBucket = new Bucket(this, 'website-bucket', {
             bucketName: 'www.openconstructfoundation.org',
-            websiteIndexDocument: 'index.html',
-            websiteErrorDocument: 'index.html'
+            blockPublicAccess:{
+                blockPublicAcls: true,
+                blockPublicPolicy: true,
+                ignorePublicAcls: true,
+                restrictPublicBuckets: true
+            }
         });
 
-        this.redirectBucket = new Bucket(this, 'redirect-bucket', {
-            bucketName: 'openconstructfoundation.org',
-            websiteRedirect: {hostName: 'www.openconstructfoundation.org', protocol: RedirectProtocol.HTTP}
-        });
+        this.oai = new OriginAccessIdentity(this, 'oai', {});
     }
 
     setupRoute53() {
+        let domainName = "openconstructfoundation.org";
+        let SAN = `www.${domainName}`;
+
         const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'zone', {
-            hostedZoneId: 'Z5F4VBVHHUZME',
-            zoneName: 'openconstructfoundation.org'
+            hostedZoneId: this.props.hostedZoneId,
+            zoneName: domainName
         });
 
-        
+        const cert = new DnsValidatedCertificate(this, 'cert', {
+            hostedZone,
+            domainName,
+            subjectAlternativeNames: [SAN]
+        });
+
+        this.distribution = new CloudFrontWebDistribution(this, 'distribution', {
+            viewerCertificate: ViewerCertificate.fromAcmCertificate(cert, {aliases: [domainName, SAN]}),
+
+            originConfigs: [
+                {
+                    s3OriginSource: {
+                        s3BucketSource: this.websiteBucket,
+                        originAccessIdentity: this.oai
+                    },
+                    behaviors: [{isDefaultBehavior: true}]
+                }
+            ]
+        });
+
 
         new RecordSet(this, 'website-dns-root', {
             recordType: RecordType.A,
             recordName: '@',
             zone: hostedZone,
-            target: RecordTarget.fromAlias(new BucketWebsiteTarget(this.redirectBucket))
+            target: RecordTarget.fromAlias(new CloudFrontTarget(this.distribution))
         });
-
         new RecordSet(this, 'website-dns', {
             recordType: RecordType.A,
             recordName: 'www',
             zone: hostedZone,
-            target: RecordTarget.fromAlias(new BucketWebsiteTarget(this.websiteBucket))
+            target: RecordTarget.fromAlias(new CloudFrontTarget(this.distribution))
         });
+        //
+        // new RecordSet(this, 'website-dns-root', {
+        //     recordType: RecordType.A,
+        //     recordName: '@',
+        //     zone: hostedZone,
+        //     target: RecordTarget.fromAlias(new BucketWebsiteTarget(this.redirectBucket))
+        // });
+        //
+        // new RecordSet(this, 'website-dns', {
+        //     recordType: RecordType.A,
+        //     recordName: 'www',
+        //     zone: hostedZone,
+        //     target: RecordTarget.fromAlias(new BucketWebsiteTarget(this.websiteBucket))
+        // });
     }
 
     private setupCodePipeline() {
@@ -72,7 +119,7 @@ export class InfraStack extends Stack {
         let compiledSite = new Artifact("built-site");
 
         const project = new PipelineProject(this, `ocf-build-project`, {
-            buildSpec: BuildSpec.fromObject(this.buildSpec),
+            buildSpec: BuildSpec.fromSourceFilename("buildspec.yml"),
             environment: {
                 buildImage: LinuxBuildImage.AMAZON_LINUX_2_2,
                 computeType: ComputeType.SMALL,
@@ -84,9 +131,7 @@ export class InfraStack extends Stack {
             actionName: 'copy-files',
             bucket: this.websiteBucket!,
             input: compiledSite,
-            runOrder: 1,
-            // @ts-ignore
-            accessControl: "public-read"
+            runOrder: 1
         });
 
         new Pipeline(this, "build-pipeline", {
@@ -131,7 +176,7 @@ export class InfraStack extends Stack {
                     "runtime-versions": {
                         "nodejs": "12"
                     },
-                    commands:[
+                    commands: [
                         `curl -O -L https://github.com/gohugoio/hugo/releases/download/v0.63.1/hugo_0.63.1_Linux-64bit.tar.gz`,
                         `tar -xvf hugo*.tar.gz hugo`,
                         `mv hugo /usr/local/bin`
