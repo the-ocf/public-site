157279bb86a4b4379a0e43b5d3a59baddca85f59
Matthew Bonig <matthew.bonig@gmail.com>
2024-02-09 12:21:52 -0700

    fixing tests

--- infra/test/main.test.ts
+++ infra/test/main.test.ts
@@ -1,11 +1,16 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 
-import { PipelineStack } from '../src/pipelineStack';
+import { WebsiteStack } from '../src/WebsiteStack';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new PipelineStack(app, 'test');
+  const stack = new WebsiteStack(app, 'test', {
+    env: {
+      account: '123456789012',
+      region: 'us-east-1',
+    },
+  });
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
