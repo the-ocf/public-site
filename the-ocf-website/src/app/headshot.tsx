00ef44b9b215d0d6ccb3748b3d2e96dfaf3f93f9
Roman Filippov <rf@romanfilippov.com>
2024-02-12 09:56:06 +0000

    fix: headshot scaling

--- the-ocf-website/src/app/headshot.tsx
+++ the-ocf-website/src/app/headshot.tsx
@@ -10,24 +10,27 @@ export function Headshot({ image, name, title, link }: {
   link: string
 }) {
   return (
-    <div className='w-full md:w-1/2 lg:w-1/3 px-4 mb-12'>
-      <div className='max-w-max'>
+    <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-12">
+      <div className="max-w-max mx-auto">
         <Image
           src={image}
           alt={`${title} - ${name}`}
-          className='object-cover h-52 w-52 mb-2'
+          className='object-cover aspect-square block mb-8'
         />
-        <h3 className='mb-1 text-3xl md:text-3xl leading-tight font-semibold'>
+
+        <h3 className="mb-2 text-3xl md:text-4xl leading-tight font-semibold">
           {name}
         </h3>
-        <div className='mb-2 text-lg font-medium text-green-500'>
+
+        <span className="text-lg font-medium text-green-500">
           {title}
+        </span>
+
+        <div className='mt-3 items-center'>
+          <a className="inline-block text-coolGray-300 hover:text-coolGray-400" href={link}>
+            <LinkedInLogoIcon width={22} height={22} />
+          </a>
         </div>
-        <div className='items-center'>
-            <a className="inline-block text-coolGray-300 hover:text-coolGray-400" href={link}>
-              <LinkedInLogoIcon width={22} height={22} />
-            </a>
-          </div>
       </div>
     </div>
   )
