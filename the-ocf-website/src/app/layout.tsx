95ebb8e02fafd8cf5edfaa09581317b856fd14b2
Roman Filippov <rf@romanfilippov.com>
2024-02-12 09:56:15 +0000

    feat: adds default body classes

--- the-ocf-website/src/app/layout.tsx
+++ the-ocf-website/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={[font.className, 'antialiased', 'font-body', 'bg-body', 'text-body'].join(' ')}>{children}</body>
     </html>
   );
 }
